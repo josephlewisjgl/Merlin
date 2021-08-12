ed0bdc17646f5ed33956790588aeff07ac356f70
josephlewisjgl <josephgeorgelewis2000@gmail.com>
2021-08-12 22:20:45 +0100

    Add get data function

--- merlin.py
+++ merlin.py
@@ -44,8 +44,8 @@ class MerlinBot():
         elif response.startswith('download'):
             self.download_data(response[9:])
 
-        elif response.startswith('list dimensions for'):
-            self.list_dimensions(response[20:])
+        elif response.startswith('get data for'):
+            self.get_data(response[13:])
 
         else:
             self.output_message("Sorry I'm not sure what you need. Could you please try asking again or refer to the "
@@ -105,12 +105,65 @@ class MerlinBot():
 
         return None
 
-    def list_dimensions(self, cube):
+    def get_data(self, cube):
         df = statswales_get_dataset(cube)
+
+        # set up number of dimensions to be incremented and the col_output_input dict to store the names of the col
+        # and the name to output as a user friendly name
+        num_dimensions = 0
+        col_output_input = {}
+
+        self.output_message('Dimensions for this cube are:')
 
         for col in df:
             if 'ItemName_ENG' in col:
-                self.output_message(col.split('_')[0])
+
+                user_friendly_col = col.split('_')[0]
+                col_output_input[user_friendly_col]= col
+                self.output_message(user_friendly_col)
+                num_dimensions += 1
+
+        self.output_message('Now we can start filtering. You can filter up to three dimensions. Please just enter one filter on each dimension for now.')
+        first = input('Select the first dimension you would like to filter: ')
+        dimension = col_output_input.get(first)
+        self.output_message('You have chosen to filter: ' + first + 'How would you like to filter this dimension, the options are below: ')
+        filter_options = df.groupby(dimension).count().reset_index()[dimension].to_list()
+        for option in filter_options:
+            self.output_message(option)
+        filter = input('What would you like to filter this column to?')
+        df = df[df[dimension] == filter]
+
+
+        if num_dimensions > 1:
+            second = input('Select the second dimension you would like to filter (type no to filter by only one dimension): ')
+            if second != 'no':
+                dimension = col_output_input.get(second)
+                self.output_message(
+                    'You have chosen to filter: ' + first + 'How would you like to filter this dimension, the options are below: ')
+                filter_options = df.groupby(dimension).count().reset_index()[dimension].to_list()
+                for option in filter_options:
+                    self.output_message(option)
+                filter = input('What would you like to filter this column to?')
+                df = df[df[dimension] == filter]
+
+        if num_dimensions > 2:
+            third = input('Select the third dimension you would like to filter (type no to filter by only two dimensions): ')
+            if third != 'no':
+                dimension = col_output_input.get(third)
+                self.output_message(
+                    'You have chosen to filter: ' + first + 'How would you like to filter this dimension, the options are below: ')
+                filter_options = df.groupby(dimension).count().reset_index()[dimension].to_list()
+                for option in filter_options:
+                    self.output_message(option)
+                filter = input('What would you like to filter this column to?')
+                df = df[df[dimension] == filter]
+
+        self.output_message(df.Data.sum())
+
+
+
+
+
 
         return None
 
